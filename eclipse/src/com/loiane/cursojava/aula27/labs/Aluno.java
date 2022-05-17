27811ae376d1c4c6aefaffa49107f9a1d207507c
Elthon Rodrigues <elthon.c.r@gmail.com>
2022-05-16 23:43:01 -0300

    Implementando Classe Aluno e Classe Teste chamada Exer03

---
+++ eclipse/src/com/loiane/cursojava/aula27/labs/Aluno.java
@@ -0,0 +1,11 @@
+package com.loiane.cursojava.aula27.labs;
+
+public class Aluno {
+
+	String nome;
+	String matricula;
+	String cursoMatriculado;
+	String[] disciplinas;
+	double[] notas;
+	
+}
---
+++ eclipse/src/com/loiane/cursojava/aula27/labs/Exer03.java
@@ -0,0 +1,24 @@
+package com.loiane.cursojava.aula27.labs;
+
+public class Exer03 {
+
+	public static void main(String[] args) {
+
+		Aluno aluno1 = new Aluno();
+		
+		aluno1.nome = "Elthon";
+		
+		aluno1.disciplinas = new String[10];
+		
+		aluno1.disciplinas[0] = "Redes de Computadores";
+		aluno1.disciplinas[1] = "Projeto e Analise de Algoritimos I";
+		aluno1.disciplinas[2] = "Inteligencia Artificial";
+		
+		aluno1.notas = new double[10];
+		
+		aluno1.notas[0] = 1;
+		
+		
+	}
+
+}
