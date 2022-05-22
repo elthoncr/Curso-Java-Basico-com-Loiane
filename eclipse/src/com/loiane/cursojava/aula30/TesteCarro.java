ea94bbcd430a0c0bd11ad5d76a72816ea09b3ba6
Elthon Rodrigues <elthon.c.r@gmail.com>
2022-05-22 00:29:20 -0300

    Aula 30 - Palavra chave this

---
+++ eclipse/src/com/loiane/cursojava/aula30/TesteCarro.java
@@ -0,0 +1,17 @@
+package com.loiane.cursojava.aula30;
+
+public class TesteCarro {
+
+	public static void main(String[] args) {
+		
+		//Carro carro1 = new Carro("Volkswagen","Fusca",5);
+		
+		Carro carro2 = new Carro("Fiat","Ducato");
+		
+		System.out.println("Marca: " + carro2.marca + "\nModelo: " + carro2.modelo + "\nnumPassag: " + carro2.numPassageiros);
+		
+		//System.out.println("Marca: " + carro1.marca + "\nModelo: " + carro1.modelo + "\nnumPassag: " + carro1.numPassageiros);
+
+	}
+
+}
